167e4019bec054ee068031329e34cde6954ce388
asimboga <asimboga@gmail.com>
2022-07-02 17:27:45 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import icon1 from "./assets/email.svg"
+import icon2 from "./assets/location.svg"
+import icon3 from "./assets/phone.svg"
 
 function App() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    handleClick()
+  }, [])
+  const handleClick = () => {
+    fetch("https://randomuser.me/api/")
+      .then((response)=> response.json())
+      .then((data)=>{
+        setData(data.results)
+        data.map((person)=>{
+          return(
+            
+          )
+        })
+      })
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container-App">
+      <div className='card'>
+        <div className='profile'>
+          <img className='img' src={} alt="" />
+          <h3 className='name'></h3>
+        </div>
+        <div className='email'>
+          <img className='icon' src={icon1} alt="" />
+          <p className='content'></p>
+        </div>
+        <div className='phone'>
+          <img className='icon' src={icon3} alt="" />
+          <p className='content'></p>
+        </div>
+        <div className='location'>
+          <img className='icon' src={icon2} alt="" />
+          <p className='content'></p>
+        </div>
+        <div>
+          <p className='age'></p>
+          <p className='date'></p>
+        </div>
+      </div>
+        <div className="card-btn">
+        <button className="btn" onClick={handleClick}>Random User</button>
+        </div>
     </div>
   );
 }
